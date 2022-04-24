4effe5fe5c2984730275d0c9881a7786a9a8dc6b
palasa <170394205@qq.com>
2022-04-24 16:30:18 +0800

    add animation

--- src/screens/main-screen.tsx
+++ src/screens/main-screen.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useState } from 'react'
 import { Text, Box, Center, VStack, themeTools, useTheme, useColorMode, useColorModeValue } from 'native-base'
-// import AnimatedCheckbox from '../components/animated-checkbox'
+import AnimatedCheckbox from '../components/animated-checkbox'
 import ThemeToggle from '../components/theme-toggle'
 
 export default function MainScreen() {
@@ -8,7 +8,7 @@ export default function MainScreen() {
     <Center _dark={{bg: 'blueGray.900'}} _light={{bg: 'blueGray.50'}} px={4} flex={1}>
       <VStack space={5} alignItems="center">
         <Box w={100} h={100}>
-          {/* <AnimatedCheckbox /> */}
+          <AnimatedCheckbox/>
         </Box>
         <Box p={10} bg={useColorModeValue('red.500','yellow.500')}>
           <Text>Hello World</Text>
